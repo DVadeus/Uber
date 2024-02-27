9a00aea4b15dc9689d5550bf548bd831b2946a7a
Dave <jdaviduarte@gmail.com>
2024-02-26 22:06:19 -0500

    commit

--- app.py
+++ app.py
@@ -86,8 +86,7 @@ df = load_df()
 st.write(df.head())
 
 df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
